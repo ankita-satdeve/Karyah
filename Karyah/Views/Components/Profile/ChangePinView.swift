937163d83ca1725ec8fd55754e08fd4da1a75087
Ankita Satdeve <ankitasatdeve03@gmail.com>
2025-02-15 11:57:05 +0530

    Profile update

---
+++ Karyah/Views/Components/Profile/ChangePinView.swift
@@ -0,0 +1,18 @@
+//
+//  ChangePinView.swift
+//  Karyah
+//
+//  Created by Prance Studio on 15/02/25.
+//
+
+import SwiftUI
+
+struct ChangePinView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangePinView()
+}
