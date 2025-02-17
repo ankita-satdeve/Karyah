8f44dcdf4163ea454d96fad0cff73762f39f3fc0
Ankita Satdeve <ankitasatdeve03@gmail.com>
2025-02-17 11:19:49 +0530

    change in dawer UI

---
+++ Karyah/Views/Components/Profile/ChangePinDrawer.swift
@@ -0,0 +1,18 @@
+//
+//  ChangePinDrawer.swift
+//  Karyah
+//
+//  Created by Prance Studio on 17/02/25.
+//
+
+import SwiftUI
+
+struct ChangePinDrawer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangePinDrawer()
+}
