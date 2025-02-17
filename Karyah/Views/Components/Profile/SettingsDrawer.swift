448d78a1f8224433e72144bac424c8346c1819c7
Ankita Satdeve <ankitasatdeve03@gmail.com>
2025-02-17 11:26:59 +0530

    another drawer added

---
+++ Karyah/Views/Components/Profile/SettingsDrawer.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsDrawer.swift
+//  Karyah
+//
+//  Created by Prance Studio on 17/02/25.
+//
+
+import SwiftUI
+
+struct SettingsDrawer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsDrawer()
+}
