bc1487a22fbe49bd94016b225015bb5caa902299
Ankita Satdeve <ankitasatdeve03@gmail.com>
2025-02-20 15:42:17 +0530

    Updated UI of Project details

---
+++ Karyah/Views/Components/Project/ProjectDetailDescriptionRow.swift
@@ -0,0 +1,18 @@
+//
+//  ProjectDetailDescriptionRow.swift
+//  Karyah
+//
+//  Created by Prance Studio on 20/02/25.
+//
+
+import SwiftUI
+
+struct ProjectDetailDescriptionRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProjectDetailDescriptionRow()
+}
