c5d518c2543982e2b50f65bd4eb804c4313f8f56
Ankita Satdeve <ankitasatdeve03@gmail.com>
2025-02-21 18:03:57 +0530

    updated ui of project list

---
+++ Karyah/Views/Components/Project/TaskBottomDrawerView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskBottomDrawerView.swift
+//  Karyah
+//
+//  Created by Prance Studio on 21/02/25.
+//
+
+import SwiftUI
+
+struct TaskBottomDrawerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskBottomDrawerView()
+}
