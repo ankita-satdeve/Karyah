da578a08127df5dc9c39f4b3bce931d54b3f1cb7
Ankita Satdeve <ankitasatdeve03@gmail.com>
2025-02-22 16:25:03 +0530

    updateProject API call

---
+++ Karyah/Views/Screens/UpdateProjectView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateProjectView.swift
+//  Karyah
+//
+//  Created by Prance Studio on 22/02/25.
+//
+
+import SwiftUI
+
+struct UpdateProjectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateProjectView()
+}
